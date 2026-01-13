e156106746a8664dd2fd8c9d14b6b45200c244b2
Lovys-prots66 <lovys6027@gmail.com>
2026-01-13 09:06:00 +0100

    Add gathering of files and folders

--- index.cjs
+++ index.cjs
@@ -1,4 +1,6 @@
-const rl = require("node:readline/promises");
+const rl = require("readline/promises");
+const fs = require("fs/promises")
+const path = require("path");
 
 async function requestPreference(question = ""){
     const prompt = rl.createInterface({
@@ -10,3 +12,19 @@ async function requestPreference(question = ""){
 
     return input;
 }
+
+async function gatherExts(dir = ""){
+    try {
+        const all = await fs.readdir(dir, { recursive : true });
+
+        let files;
+
+        // await Promise.all([])
+
+        return all;
+    } catch (error) {
+        throw new Error(error.message);
+    }
+}
+
+console.log(gatherExts("C:\\Users\\lenovo\\Desktop\\ctrl regio").then(console.log)); 
