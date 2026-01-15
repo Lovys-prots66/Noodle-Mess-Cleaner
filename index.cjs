b5324da1d3b9f2593d563da9b3fb5359094724ea
Lovys-prots66 <lovys6027@gmail.com>
2026-01-15 21:15:06 +0100

    Add obj of available exts

--- index.cjs
+++ index.cjs
@@ -2,6 +2,13 @@ const rl = require("readline/promises");
 const fs = require("fs/promises")
 const path = require("path");
 
+const matches = {
+    images : ['.png', '.jpg', '.jpeg', '.gif'],
+    medias : ".mp3",
+    documents : ['.pdf', '.docx', '.pptx', '.xlsx', '.xls'],
+    code : [".html", ".css", 'js', ".jsx", ".ts", "tsx", ".php", '.c', 'cpp']
+}
+
 async function requestPreference(question = ""){
     const prompt = rl.createInterface({
         input : process.stdin,
