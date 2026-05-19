5009ab678cf1bccec18cc5496e0a8685197e243c
Lovys-prots66 <lovys6027@gmail.com>
2026-05-19 21:27:37 +0100

    use cwd as cleanup target

--- utils/Dispatch.cjs
+++ utils/Dispatch.cjs
@@ -1,20 +1,14 @@
 const requestPreference = require("../helpers/requestPreference.cjs");
 const Cleaner = require("./cleanUp.cjs");
 
-const fs = require("fs/promises");
 const dirAccessible = require("../helpers/dirAccessible.cjs");
 
 module.exports = async function run(){
-    let dir;
-
-    do {
-        dir = await requestPreference("Which directory you wish to cleanup?\n-->");
-        if(!dir || !await dirAccessible(dir)) console.log("Please provide a proper directory.");    
-    } while (!dir || !await dirAccessible(dir));
+    const dir = process.cwd();
 
     let cleanupType;
     do {
-        cleanupType = await requestPreference("Which method you would like the cleanup to be done.\n\t[1] : In a new directory\n\t[2] : Just whithin the provided directory.\n\n-->");
+        cleanupType = await requestPreference("Which method you would like the cleanup to be done.\n\t[1] : In a new directory\n\t[2] : Just whithin the current directory.\n\n-->");
         if(!cleanupType || (cleanupType != 1 && cleanupType != 2)){
             console.log("Please provide a proper answer.");
         }
@@ -35,11 +29,10 @@ module.exports = async function run(){
             }
         } while (!targetDir);
 
-        cleaner.cleanupTypeOne(dir = dir, targetDir = targetDir);
+        await cleaner.cleanupTypeOne(dir, targetDir);
     }else if(cleanupType == 2){
-        cleaner.cleanupTypeTwo(dir);
+        await cleaner.cleanupTypeTwo(dir);
     }
 
     console.log("\nCleanup complete. Exiting...");
-    process.exit(0);
 }
